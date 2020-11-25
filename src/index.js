faf92b17ee4714d34be7184879459469ca726508
“saschakala” <mxsaschakala@gmail.com>
2020-11-25 17:58:34 -0500

    commit tree 462d9c91f8bb62c11e9bd39b53e599dc459927e4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetchPics()
+
+
+    function fetchPics() {
+        return fetch (imgUrl)
+        .then (resp => resp.json())
+        .then (json => addPicsToPage(json)
+        )}
+
+    function addPicsToPage(pics){
+        const imageContainer = document.getElementById('dog-image-container')
+        pics.message.forEach(function(url) {
+            const imageElement = document.createElement('img')
+            imageElement.src = url
+            imageElement.width = "300"
+            imageContainer.appendChild(imageElement)
+        })
+    }
+
+
+
+  });
+
